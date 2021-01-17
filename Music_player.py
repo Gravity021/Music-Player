a8d64fb98bd7d60dca9f162d1b433250a844c176
Isaac 45678 <hero45678@hotmail.com>
2021-01-17 11:34:24 +0000

    Bug fixes. Update README.md.

--- Music_player.py
+++ Music_player.py
@@ -3,6 +3,7 @@ import os
 import json
 from ctypes import windll
 import getpass
+
 from Data.font import Font
 
 SetWindowPos = windll.user32.SetWindowPos
@@ -38,9 +39,12 @@ def scan():
                         name = " ".join(file.split("_")).split(".")[0]
                         sound = pygame.mixer.Sound(os.path.join(root, file))
                         try:
-                            image = pygame.image.load(os.path.join(root, dir +".jpg"))
+                            try:
+                                image = pygame.image.load(os.path.join(root, dir +".jpg"))
+                            except Exception:
+                                image = pygame.image.load(os.path.join(root, dir +".png"))
                         except Exception:
-                            image = pygame.image.load(os.path.join(root, dir +".png"))
+                            image = None
                         songs.append([name, dir, sound, image])
                 
     if len(songs) != 0:
@@ -53,22 +57,41 @@ while True:
     screen.fill((255, 255, 255))
     events = pygame.event.get()
 
-    pygame.draw.polygon(screen, (0, 0, 0), [[0, 0], [299, 0], [299, 99], [0, 99]], 1)
-
-    song = songs[song_no]
-    font.render(screen, f"{song[0]}", font.centre(f"{song[0]}", (100, 40)))
-    font.render(screen, f"{song[1]}", font.centre(f"{song[1]}", (100, 60)))
-    font.render(screen, f"{volume}", font.centre(f"{volume}", (180, 90)))
-    
-    screen.blit(pygame.transform.scale(songs[song_no][3], (100, 100)), (200, 0))
+    pygame.draw.polygon(screen, (0, 0, 0), [[0, 0], [199, 0], [199, 99], [0, 99]], 1)
+    pygame.draw.polygon(screen, (0, 0, 0), [[199, 0], [299, 0], [299, 99], [199, 99]], 1)
 
     if not pygame.mixer.get_busy():
-        if song_no < len(songs) -1:
+        if song_no < len(songs) - 1:
             song_no += 1
         else:
             song_no = 0
         
-        channel.play(songs[0][2])
+        channel.play(songs[song_no][2])
+
+    song = songs[song_no]
+    if len(song[0]) > 30:
+        name = [song[0][:len(song[0]) // 2], song[0][len(song[0]) // 2:]]
+        if len(name[0]) < 20:
+            font.render(screen, f"{name[0]}", font.centre(f"{name[0]}", (100, 40)))
+            font.render(screen, f"{name[1]}", font.centre(f"{name[1]}", (100, 60)))
+            font.render(screen, f"{song[1]}", font.centre(f"{song[1]}", (100, 80)))
+        else:
+            name = [song[0][: len(song[0]) // 3], song[0][len(song[0]) // 3 : (len(song[0]) // 3) * 2], song[0][(len(song[0]) // 3) * 2 :]]
+            font.render(screen, f"{name[0]}", font.centre(f"{name[0]}", (100, 20)))
+            font.render(screen, f"{name[1]}", font.centre(f"{name[1]}", (100, 40)))
+            font.render(screen, f"{name[2]}", font.centre(f"{name[2]}", (100, 60)))
+            font.render(screen, f"{song[1]}", font.centre(f"{song[1]}", (100, 80)))
+    else:
+        font.render(screen, f"{song[0]}", font.centre(f"{song[0]}", (100, 40)))
+        font.render(screen, f"{song[1]}", font.centre(f"{song[1]}", (100, 60)))
+
+    font.render(screen, f"{volume}", font.centre(f"{volume}", (180, 90)))
+    
+    try:
+        screen.blit(pygame.transform.scale(songs[song_no][3], (100, 100)), (200, 0))
+    except Exception:
+        font.render(screen, "Img not", font.centre("Img not", (250, 40)))
+        font.render(screen, "found", font.centre("found", (250, 60)))
 
     for event in events:
         if event.type == pygame.MOUSEBUTTONUP:
@@ -97,6 +120,25 @@ while True:
                 elif event.key == pygame.K_r:
                     pygame.mixer.stop()
                     songs, song_no = scan()
+            
+            elif event.key == pygame.K_UP:
+                if volume < 100:
+                    volume += 5
+                    channel.set_volume(volume / 100)
+            
+            elif event.key == pygame.K_DOWN:
+                if volume > 0:
+                    volume -= 5
+                    channel.set_volume(volume / 100)
+            
+            elif event.key == pygame.K_RIGHT:
+                channel.stop()
+                if song_no < len(songs) - 1:
+                    song_no += 1
+                else:
+                    song_no = 0
+        
+                channel.play(songs[song_no][2])
         
         elif event.type == pygame.KEYUP:
             if event.key == pygame.K_LCTRL:
--- README.md
+++ README.md
@@ -4,6 +4,22 @@ This is a small project to play music. Simple!
 
 ## Features
 
-Simple display box (at a position of your choice) without a frame. Finds songs from a set location. Displays the song name, and a cover image (you need to download to the same location as the song).
+Simple display box (at a position of your choice) without a frame. Finds songs from a set location. Displays the song name, and a cover image (you need to download to the same location as the song). Volume adjusted through scrolling with mouse wheel.
 
 All setings set through the "config.json" file
+
+## How to set up
+
+### Configuration
+1) Open the "Data/config.json" file
+2) Set the settings to your preference
+3) Save the file
+
+### Music directory
+
+Folder structure should be:
+"play_folder" 
+|- "Album 1"
+    |- Album cover image ("album name .png/.jpg") - NOTICE: This file is not necessary but recomended
+    |- Songs (.mp3/.wav)
+|- "Album 2" etc.
